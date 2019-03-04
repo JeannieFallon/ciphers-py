71739740d2fc1a018cd4cba2046141caaea9bcfd
JeannieFallon <jeanniefallon@gmail.com>
2019-03-04 17:10:29 -0500

    stub out main

---
+++ main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 16:40:16 2019
+
+@author: jfall
+"""
+
+# Basic CLI cipher application
+
+### FUNCTIONS ###
+
+def getCaesarCipher(plnStr):
+    # method stub
+    ciphStr = "This is an output cipher string."
+    return ciphStr
+
+
+### MAIN PROGRAM ###
+    
+plnStr = "This is an input plain string."
+ciphStr = getCaesarCipher(plnStr)
+print("Plain string: " + plnStr)
+print("Cipher string: " + ciphStr)
