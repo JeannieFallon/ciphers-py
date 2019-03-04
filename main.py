9e5668827453c9b1f30a430f6e8168b437895980
JeannieFallon <jeanniefallon@gmail.com>
2019-03-04 17:27:37 -0500

    add cipher file

--- main.py
+++ main.py
@@ -5,19 +5,26 @@ Created on Mon Mar  4 16:40:16 2019
 @author: jfall
 """
 
+import cipher
+
 # Basic CLI cipher application
 
 ### FUNCTIONS ###
 
-def getCaesarCipher(plnStr):
+def getCaesar(plnStr):
     # method stub
-    ciphStr = "This is an output cipher string."
+    ciphStr = cipher.getSingleShift(plnStr)
     return ciphStr
 
+def getVigenere(plnStr):
+    # method stub
+    ciphStr = cipher.getMultiShift(plnStr)
+    return ciphStr
 
 ### MAIN PROGRAM ###
     
 plnStr = "This is an input plain string."
-ciphStr = getCaesarCipher(plnStr)
+
 print("Plain string: " + plnStr)
-print("Cipher string: " + ciphStr)
+print("Single-shift cipher string: " + getCaesar(plnStr))
+print("Multi-shift cipher string: " + getVigenere(plnStr))
