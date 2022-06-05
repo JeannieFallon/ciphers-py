efd02c09f504bff91abdcce99ee276f187448469
Jeannie Fallon <jeanniefallon@gmail.com>
2022-06-05 15:09:48 -0400

    add argparse & subcmd structure

--- src/ciphers.py
+++ src/ciphers.py
@@ -1,9 +1,27 @@
 import sys
+import argparse
+
+
+def rot13(args) -> int:
+    plain = args.plaintext
+    print(f'Running ROT13 cipher on plaintext: {plain}')
+
+    return 0
 
 
 def main() -> int:
-    print('Running ciphers-py')
+    parser = argparse.ArgumentParser('main parser')
+    cipher_parsers = parser.add_subparsers(dest='cipher', required=True, help='ciphers parsers')
+
+    rot13_parser = cipher_parsers.add_parser('rot13', help='rot13 help')
+    rot13_parser.add_argument('plaintext', help='plain text to be enciphered')
+    rot13_parser.set_defaults(func=rot13)
+
+    args = parser.parse_args()
+    args.func(args)
+
     return 0
 
+
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
--- test/test_ciphers.py
+++ test/test_ciphers.py
@@ -1,10 +1,26 @@
 import sys
+import errno
 
 import pytest
 
 import src.ciphers
 
+
+class Args:
+    def __init__(self, *, plaintext=None):
+        self.plaintext = plaintext
+
+
 class TestCiphers:
-    def test_ciphers(self):
-        rc = src.ciphers.main()
+    def test_noarg(self):
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            rc = src.ciphers.main()
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == errno.ENOENT
+
+    def test_rot13(self):
+        plain = 'This is test plaintext'
+        args = Args(plaintext=plain)
+        rc = src.ciphers.rot13(args)
         assert rc == 0
+        # TODO get console output
