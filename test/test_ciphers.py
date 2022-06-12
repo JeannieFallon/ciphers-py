b757ea86758bd81c036479025a6fcaf26728254f
Jeannie Fallon <jeanniefallon@gmail.com>
2022-06-12 15:57:54 -0400

    add keyvals test

--- test/test_ciphers.py
+++ test/test_ciphers.py
@@ -12,6 +12,17 @@ TEST_MESSAGES = [
     ("l! M@ n#", "y! Z@ a#"),
     ("1 2 3 4 5", "1 2 3 4 5"),
 ]
+# TODO block keyval % 26 = 0
+TEST_KEYVALS = [
+    (0, "aBc XyZ"),
+    (1, "bCd YzA"),
+    (10, "kLm HiJ"),
+    (25, "zAb WxY"),
+    (26, "aBc XyZ"),
+    (42, "qRs NoP"),
+]
+
+DEFAULT_MESSAGE = "aBc XyZ"
 DEFAULT_KEYVAL = 13
 
 
@@ -53,6 +64,18 @@ class TestCaesar:
         output = capsys.readouterr()
         assert message in output.out
         assert f"{DEFAULT_KEYVAL}" in output.out
+        assert cipher in output.out
+
+    @pytest.mark.parametrize("keyval,cipher", TEST_KEYVALS)
+    def test_keyvals(self, capsys, keyval, cipher):
+        args = Args(message=DEFAULT_MESSAGE, keyval=keyval)
+
+        rc = src.ciphers.caesar(args)
+        assert rc == 0
+
+        output = capsys.readouterr()
+        assert DEFAULT_MESSAGE in output.out
+        assert f"{keyval}" in output.out
         assert cipher in output.out
 
 
